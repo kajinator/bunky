a7fd2b7ee3441937ea4180ee93e200d6139baf76
kajinator <105234609+kajinator@users.noreply.github.com>
2023-12-24 22:31:38 -0800

    feat: error logging

--- src/index.js
+++ src/index.js
@@ -26,6 +26,36 @@ for (const folder of commandFolders) {
     }
 }
 
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(
+            `No command matching ${interaction.commandName} was found.`
+        );
+        return;
+    }
+
+    try {
+        await command.execute(client, interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({
+                content: 'There was an error while executing this command.',
+                ephemeral: true,
+            });
+        } else {
+            await interaction.reply({
+                content: 'There was an error while executing this command.',
+                ephemeral: true,
+            });
+        }
+    }
+});
+
 client.once(Events.ClientReady, (readyClient) => {
     console.log(
         `Logged in as ${readyClient.user.tag}. Initial roundtrip: ${client.ws.ping}`
