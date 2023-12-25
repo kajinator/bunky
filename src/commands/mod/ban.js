27ad58f80bbb8def10f5d878bb59b953aa443a9c
kajinator <105234609+kajinator@users.noreply.github.com>
2023-12-24 22:37:47 -0800

    feat: moderation kick and ban commands

---
+++ src/commands/mod/ban.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ban')
+        .setDescription('Bans a user from the server')
+        .addUserOption((option) =>
+            option
+                .setName('target')
+                .setDescription('Select a target to ban')
+                .setRequired(true)
+        )
+        .addStringOption((option) =>
+            option
+                .setName('reason')
+                .setDescription('Specified reason for the ban')
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+
+    async execute(client, interaction) {
+        const target = interaction.options.getUser('user');
+        const reason = interaction.options.getString('reason');
+        interaction.guild.members.ban(target);
+        return interaction.reply({
+            content: `${interaction.user.toString()} has banned ${target.toString()} for: ${reason}`,
+            ephemeral: false,
+        });
+    },
+};
---
+++ src/commands/mod/kick.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('kick')
+        .setDescription('Kicks a user from the server')
+        .addUserOption((option) =>
+            option
+                .setName('target')
+                .setDescription('Select a target to kick')
+                .setRequired(true)
+        )
+        .addStringOption((option) =>
+            option
+                .setName('reason')
+                .setDescription('Specified reason for the kick')
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers),
+
+    async execute(client, interaction) {
+        const target = interaction.options.getUser('user');
+        const reason = interaction.options.getString('reason');
+        interaction.guild.members.kick(target);
+        return interaction.reply({
+            content: `${interaction.user.toString()} has kicked ${target.toString()} for: ${reason}`,
+            ephemeral: false,
+        });
+    },
+};
